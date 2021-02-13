def6053bd5d77bde8116b479846a61a8037d708f
XmlEntity <byep0klgithubkre035@riseup.net>
2021-02-13 15:13:01 +0200

    add browser_path parameter and open videos in browser

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import py_cui
 from youtubesearchpython import *
+import sys
+import os
+
+all_videos = []  # global dict that contains all videos
+
+if len(sys.argv):
+    print('Usage: python3 main.py /path/to/browser/executable')
+    sys.exit()
+
+browser_path = str(sys.argv[1])
 
 
 class YtSearcher:
@@ -15,17 +25,22 @@ class YtSearcher:
         self.videos_list.add_key_command(py_cui.keys.KEY_ENTER, self.list_handler)
 
     def search_videos(self, next_page=False):
+        global all_videos
+
         query = self.search_textbox.get()
-        if not next_page:
+        if not next_page:  # if next_page is False - rewrites object
             self.search = VideosSearch(query, limit=20)
+            all_videos.clear()  # clears global dict if user enters a new query
         result = self.search.result(mode=ResultMode.dict)
-        videos = {}
+        videos = {}  # local dict to parse new videos
         for i in range(0, 20):
             try:
                 vid = result['result'][i]['id']
                 title = result['result'][i]['title']
                 videos[vid] = title
-            except Exception as ex:  # bad code
+
+                all_videos.append(vid + '-:split:-' + title)
+            except:
                 break
         if not next_page:
             self.videos_list.clear()
@@ -42,11 +57,24 @@ class YtSearcher:
                 self.search_videos(next_page=True)
             except:
                 pass
+        else:
+            for video in all_videos:
+                if selected in video:
+                    vid = video.split('-:split:-')[0]  # ugly but works
+                    os.system(browser_path + ' https://www.youtube.com/watch?v=' + vid)
 
 
 root = py_cui.PyCUI(7, 6)
+
+if browser_path == '-h' or browser_path == '--help':
+    print('Usage: python3 main.py /path/to/browser/executable')
+    sys.exit()
+elif not os.path.exists(browser_path):
+    print('Error! Path ' + browser_path + ' does not exists')
+    sys.exit()
 
 root.toggle_unicode_borders()
 root.set_title('YouTube Searcher')
 yt = YtSearcher(root)
 root.start()
+
